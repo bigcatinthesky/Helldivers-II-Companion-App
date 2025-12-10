d6279c6986009ae4b69dd91d4aa751ae2492b6d3
Parker <pbowman@ithaca.edu>
2025-12-09 21:07:01 -0500

    working planet search

--- galactic_war_web.py
+++ galactic_war_web.py
@@ -1,3 +1,6 @@
+import galactic_war_api
+
+
 class GalacticWarWeb:
     def __init__(self, galactic_war):
         self._galactic_war = galactic_war
@@ -6,3 +9,53 @@ class GalacticWarWeb:
 
     def format_galactic_war(self):
         return self._galactic_war.format_galaxy_stats(self._galactic_war_stats, self._galactic_war_mission_stats)
+
+    def get_planets_string(self, sector):
+        if sector != "":
+            planets_obj = self._galactic_war.planets_in_sector(sector)
+        else:
+            planets_obj = self._galactic_war.get_planetary_list()
+        planets_str = ''
+        index = 0
+        for i in planets_obj:
+            index += 1
+            planet_str = "("+str(index)+")\n"+i.__str__()+"\n--------------------\n"
+            planets_str += planet_str
+        return planets_str
+
+    def get_sector_string(self):
+        sectors = self._galactic_war.get_sectors()
+        sectors_str = ''
+        index = 0
+        for i in sectors:
+            index += 1
+            sector_str = "("+str(index)+")\n"+str(i)+"\n--------------------\n"
+            sectors_str += sector_str
+        return sectors_str
+
+    def name_search(self, name):
+        return self._galactic_war.planet_search_by_name(name)
+
+    def index_search(self, index):
+        return self._galactic_war.planet_search_by_index(int(index)-1)
+
+    def sector_search(self, sector):
+        return self._galactic_war.planets_in_sector(sector)
+
+
+def make_galactic_war():
+    """creates and initializes a galactic war object, converts into returned galactic war web object"""
+    galactic_war = galactic_war_api.new_galactic_war()
+    galactic_war_web = GalacticWarWeb(galactic_war)
+    return galactic_war_web
+
+def format_planet(planet):
+    helldiver_stats = planet.calculate_helldiver_stats()
+    mission_stats = planet.calculate_mission_stats(helldiver_stats)
+    return planet.format_planet_stats(helldiver_stats, mission_stats)
+
+def get_planet_name(planet):
+    return planet.get_planet_attributes()["name"]
+
+def get_planet_sector(planet):
+    return planet.get_planet_attributes()["sector"]
--- run_web.py
+++ run_web.py
@@ -1,14 +1,76 @@
-from flask import Flask, render_template
-import galactic_war_api
-from galactic_war_web import GalacticWarWeb
+from flask import Flask, render_template, request, session, redirect, url_for
+from flask_session import Session
+import galactic_war_web
 
 app = Flask(__name__, template_folder='templates')
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = 'filesystem'
+Session(app)
+
 @app.route('/')
 def main():
-    galactic_war = galactic_war_api.new_galactic_war()
-    galactic_war_web = GalacticWarWeb(galactic_war)
-    return render_template('home.html', message=galactic_war_web.format_galactic_war())
+    """home page"""
+    session.clear()
+    galactic_war = galactic_war_web.make_galactic_war()
+    if not session.get("planets"):
+        planets = galactic_war.get_planets_string("")
+        session["planets"] = planets
+    if not session.get("sectors"):
+        sectors = galactic_war.get_sector_string()
+        session["sectors"] = sectors
+    return render_template('display.html',
+                           data=galactic_war.format_galactic_war(), name="Galactic War", sector="")
+
+@app.route('/planets')
+def planet_list():
+    """presents a list of all planets in the sector or galaxy"""
+    sector = request.args.get("sector", "")
+    if sector == "":
+        planets = session.get("planets")
+        message = "All Planets"
+    else:
+        planets = session.get("planets_in_{}".format(sector))
+        message = "Planets In The {} Sector".format(sector)
+    session["search_type"] = "planet"
+    return render_template('list.html', data=planets, message=message)
+
+@app.route('/sectors')
+def sector_list():
+    """presents a list of all sectors which, when selected, display a list of planets in the sector"""
+    sectors = session.get("sectors")
+    session["search_type"] = "sector"
+    return render_template('list.html', data=sectors, message="Sectors")
+
+@app.route('/search')
+def search():
+    """searches for a planet in a list of planets, search by index or name, caps sensitive"""
+    galactic_war = galactic_war_web.make_galactic_war()
+    query = request.args.get("query", "")
+    search_type = session.get("search_type")
+    print(query)
+    if search_type == "planet":
+        if query.isdigit():
+            target_planet = galactic_war.index_search(query)
+        elif query.isalpha():
+            query.lower()
+            target_planet = galactic_war.name_search(query)
+        else:
+            return render_template('error.html', message="Invalid search character!")
+        return redirect(url_for('planet', target_planet=galactic_war_web.format_planet(target_planet),
+                                planet_name=galactic_war_web.get_planet_name(target_planet),
+                                planet_sector=galactic_war_web.get_planet_sector(target_planet)+" Sector"))
+    else:
+        return None
+
+@app.route('/planet')
+def planet():
+    """displays the given planet's statistics"""
+    target_planet = request.args.get("target_planet", "")
+    planet_name = request.args.get("planet_name", "")
+    planet_sector = request.args.get("planet_sector", "")
+    return render_template('display.html', data=target_planet, name=planet_name, sector=planet_sector)
 
 if __name__ == "__main__":
+    app.secret_key = 'squidward'
     app.run()
