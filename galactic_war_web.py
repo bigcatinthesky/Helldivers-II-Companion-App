5a8ebcba6a244ff9795df20ad27341519f44a41a
Parker <pbowman@ithaca.edu>
2025-12-09 23:48:11 -0500

    working basic name search function for planets and sectors

--- galactic_war_web.py
+++ galactic_war_web.py
@@ -1,6 +1,5 @@
 import galactic_war_api
 
-
 class GalacticWarWeb:
     def __init__(self, galactic_war):
         self._galactic_war = galactic_war
@@ -10,18 +9,11 @@ class GalacticWarWeb:
     def format_galactic_war(self):
         return self._galactic_war.format_galaxy_stats(self._galactic_war_stats, self._galactic_war_mission_stats)
 
-    def get_planets_string(self, sector):
-        if sector != "":
-            planets_obj = self._galactic_war.planets_in_sector(sector)
+    def get_planets(self, sector):
+        if sector == "":
+            return self._galactic_war.get_planetary_list()
         else:
-            planets_obj = self._galactic_war.get_planetary_list()
-        planets_str = ''
-        index = 0
-        for i in planets_obj:
-            index += 1
-            planet_str = "("+str(index)+")\n"+i.__str__()+"\n--------------------\n"
-            planets_str += planet_str
-        return planets_str
+            return self._galactic_war.planets_in_sector(sector)
 
     def get_sector_string(self):
         sectors = self._galactic_war.get_sectors()
@@ -29,19 +21,22 @@ class GalacticWarWeb:
         index = 0
         for i in sectors:
             index += 1
-            sector_str = "("+str(index)+")\n"+str(i)+"\n--------------------\n"
+            sector_str = str(i)+" Sector"+"\n--------------------\n"
             sectors_str += sector_str
         return sectors_str
 
-    def name_search(self, name):
-        return self._galactic_war.planet_search_by_name(name)
+    def get_sector_count(self):
+        return len(self._galactic_war.get_sectors())
 
-    def index_search(self, index):
-        return self._galactic_war.planet_search_by_index(int(index)-1)
+    def planet_name_search(self, name):
+        return self._galactic_war.planet_search_by_name(str(name))
 
-    def sector_search(self, sector):
-        return self._galactic_war.planets_in_sector(sector)
-
+    def sector_name_search(self, name):
+        for i in self._galactic_war.get_sectors():
+            print(i)
+            if str(name) in i:
+                return i
+        return None
 
 def make_galactic_war():
     """creates and initializes a galactic war object, converts into returned galactic war web object"""
@@ -50,12 +45,24 @@ def make_galactic_war():
     return galactic_war_web
 
 def format_planet(planet):
-    helldiver_stats = planet.calculate_helldiver_stats()
-    mission_stats = planet.calculate_mission_stats(helldiver_stats)
-    return planet.format_planet_stats(helldiver_stats, mission_stats)
+    if planet.get_planet_stats()["mission time"] > 0 and planet.get_planet_stats()["shots fired"] > 0:
+        helldiver_stats = planet.calculate_helldiver_stats()
+        mission_stats = planet.calculate_mission_stats(helldiver_stats)
+        return planet.format_planet_stats(helldiver_stats, mission_stats)
+    else:
+        return "No helldiver operations recorded on {}".format(get_planet_name(planet))
 
 def get_planet_name(planet):
     return planet.get_planet_attributes()["name"]
 
 def get_planet_sector(planet):
     return planet.get_planet_attributes()["sector"]
+
+def get_planets_string(planets):
+    planets_str = ''
+    index = 0
+    for i in planets:
+        index += 1
+        planet_str = i.__str__()+"\n--------------------\n"
+        planets_str += planet_str
+    return planets_str
--- run_web.py
+++ run_web.py
@@ -13,12 +13,12 @@ def main():
     """home page"""
     session.clear()
     galactic_war = galactic_war_web.make_galactic_war()
-    if not session.get("planets"):
-        planets = galactic_war.get_planets_string("")
-        session["planets"] = planets
-    if not session.get("sectors"):
-        sectors = galactic_war.get_sector_string()
-        session["sectors"] = sectors
+    planets = galactic_war.get_planets("")
+    session["planets"] = galactic_war_web.get_planets_string(planets)
+    session["planet_count"] = len(planets)
+    sectors = galactic_war.get_sector_string()
+    session["sectors"] = sectors
+    session["sector_count"] = galactic_war.get_sector_count()
     return render_template('display.html',
                            data=galactic_war.format_galactic_war(), name="Galactic War", sector="")
 
@@ -26,12 +26,13 @@ def main():
 def planet_list():
     """presents a list of all planets in the sector or galaxy"""
     sector = request.args.get("sector", "")
+
     if sector == "":
         planets = session.get("planets")
-        message = "All Planets"
+        message = "{} Planets".format(session.get("planet_count"))
     else:
         planets = session.get("planets_in_{}".format(sector))
-        message = "Planets In The {} Sector".format(sector)
+        message = "{} Planets In The {} Sector".format(len(planets),sector)
     session["search_type"] = "planet"
     return render_template('list.html', data=planets, message=message)
 
@@ -40,7 +41,8 @@ def sector_list():
     """presents a list of all sectors which, when selected, display a list of planets in the sector"""
     sectors = session.get("sectors")
     session["search_type"] = "sector"
-    return render_template('list.html', data=sectors, message="Sectors")
+    message = "{} Sectors".format(session.get("sector_count"))
+    return render_template('list.html', data=sectors, message=message)
 
 @app.route('/search')
 def search():
@@ -48,20 +50,39 @@ def search():
     galactic_war = galactic_war_web.make_galactic_war()
     query = request.args.get("query", "")
     search_type = session.get("search_type")
-    print(query)
     if search_type == "planet":
-        if query.isdigit():
-            target_planet = galactic_war.index_search(query)
-        elif query.isalpha():
-            query.lower()
-            target_planet = galactic_war.name_search(query)
+        query = query.title()
+        target_planet = galactic_war.planet_name_search(query)
+        if target_planet and len(target_planet) > 1:
+            planets = []
+            for i in target_planet:
+                planets.append(i)
+            session["planet_count"] = len(planets)
+            planets_str = galactic_war_web.get_planets_string(planets)
+            session["planets"] = planets_str
+            return redirect('/planets')
+        elif target_planet:
+            target_planet = target_planet[0]
+        if target_planet:
+            return redirect(url_for('planet', target_planet=galactic_war_web.format_planet(target_planet),
+                                    planet_name=galactic_war_web.get_planet_name(target_planet),
+                                    planet_sector=galactic_war_web.get_planet_sector(target_planet)+" Sector"))
         else:
-            return render_template('error.html', message="Invalid search character!")
-        return redirect(url_for('planet', target_planet=galactic_war_web.format_planet(target_planet),
-                                planet_name=galactic_war_web.get_planet_name(target_planet),
-                                planet_sector=galactic_war_web.get_planet_sector(target_planet)+" Sector"))
+            return render_template('error.html', message="Planet not found!")
+    elif search_type == "sector":
+        query = query.title()
+        print(query)
+        target_sector = galactic_war.sector_name_search(query)
+        if target_sector:
+            planets = galactic_war.get_planets(target_sector)
+            session["planets"] = galactic_war_web.get_planets_string(planets)
+            session["planet_count"] = len(planets)
+            session["sector"] = target_sector
+            return redirect('/planets')
+        else:
+            return render_template('error.html', message="Sector not found!")
     else:
-        return None
+        return render_template('error.html', message="Invalid search type!")
 
 @app.route('/planet')
 def planet():
@@ -69,7 +90,10 @@ def planet():
     target_planet = request.args.get("target_planet", "")
     planet_name = request.args.get("planet_name", "")
     planet_sector = request.args.get("planet_sector", "")
-    return render_template('display.html', data=target_planet, name=planet_name, sector=planet_sector)
+    if target_planet != "" and planet_name != "" and planet_sector != "":
+        return render_template('display.html', data=target_planet, name=planet_name, sector=planet_sector)
+    else:
+        return render_template('error.html', message="Invalid planet!")
 
 if __name__ == "__main__":
     app.secret_key = 'squidward'
